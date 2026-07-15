6b077d1833aa017494318c40f609149ef936aa35
Udit <tidu.main@gmail.com>
2026-07-15 18:49:06 +0530

    Add files via upload

---
+++ Ch 2/Q2_PS.py
@@ -0,0 +1,2 @@
+a=int(input("Enter your number:"))
+print(a%2)
---
+++ Ch 2/Variables.py
@@ -0,0 +1,10 @@
+# Variable is any container that is used to store any values 
+# Data Types are: Integers(int) , Floating point numbers (float), Strings (str) , Booleans  , None
+# In variables no white spaces and special characters are not allowed 
+# Vraiables cant start with a digit 
+# Variables can start with alphabets and underscores 
+# Type casting is the coversion a datatype into another data type 
+# for example 
+str(29)
+# it will convert integer 29 to a string "29"
+# input() is a function which allows the user to give a input to the program 
