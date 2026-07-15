1a250b673bedfb1f8a63a1e013b90a6355b87f92
Udit <tidu.main@gmail.com>
2026-07-15 18:48:41 +0530

    Add files via upload

---
+++ Ch 1/Modules.py
@@ -0,0 +1,15 @@
+# Modules are the files  containg a specific code written by usually somebody esle 
+# Pip is used to any module that is not already installed in Python 
+# External modules are installed using pip 
+
+# One example of external module is pyjokes
+import pyjokes
+print (pyjokes.get_joke())
+
+# Comments in python can be used to explain parts of a code 
+# Comments are used to write statements that we dont want to use in our program 
+# Comments are of 2 types 
+# 1. Single line (#) 
+# 2. Multi line (""")
+
+# Python can be used as a calculator with the help of REPL (Read Evaluate Print Loop)
---
+++ Ch 1/Q4_PS.py
@@ -0,0 +1,4 @@
+import os
+DIRECTORY=r'C:\Users\s\OneDrive\Desktop\PYTHON'
+CONTENTS=os.listdir(DIRECTORY)
+print(CONTENTS)
